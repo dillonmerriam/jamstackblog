83152c0577eb0fb6a75e277b2068354708995726
dillonmerriam <merriamdillon@yahoo.com>
2022-03-12 12:57:04 -0800

    setting up netlify with next

---
+++ .next/static/LKQwWQ5lHhZHo-62sHvp0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d5e119ee80b716e6.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
